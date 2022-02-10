bcca308ecdaca1cef290e833362d366f9df4e2cc
Dean Matthews <drm9114@nyu.edu>
2022-02-10 14:52:17 -0500

    rr

--- solution.py
+++ solution.py
@@ -3,7 +3,7 @@ from socket import *
 # In order to terminate the program
 import sys
 
-#Dean Reuben Matthews
+#Dean Reuben Matthews m
 def webServer(port=13331):
   serverSocket = socket(AF_INET, SOCK_STREAM)
   #Prepare a server socket
