5abe79800d65bf9223ad2bed8984029c92ec789e
Dean Matthews <drm9114@nyu.edu>
2022-02-11 14:17:29 -0500

    almost done

--- solution.py
+++ solution.py
@@ -3,52 +3,64 @@ from socket import *
 # In order to terminate the program
 import sys
 
-#Dean Reuben Matthews m
 def webServer(port=13331):
   serverSocket = socket(AF_INET, SOCK_STREAM)
   #Prepare a server socket
-  serverSocket.bind(("", port))
+  serverSocket.bind(("127.0.0.1", port))
   #Fill in start
-
+  serverSocket.listen(1)
   #Fill in end
 
   while True:
     #Establish the connection
-    #print('Ready to serve...')
-    connectionSocket, addr = #Fill in start      #Fill in end
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept() #Fill in start      #Fill in end
     try:
 
       try:
-        message = #Fill in start    #Fill in end
+        message = connectionSocket.recv(1024).decode() #Fill in start    #Fill in end
         filename = message.split()[1]
         f = open(filename[1:])
-        outputdata = #Fill in start     #Fill in end
-        
+        #print('file open...')
+        outputdata = f.read() #Fill in start     #Fill in end
+        #print(outputdata)
         #Send one HTTP header line into socket.
         #Fill in start
-
+        http_str = "HTTP/1.1 200 OK\r\n\r\n"
+        #print(http_str)
+        connectionSocket.send(http_str.encode())
         #Fill in end
+        #print("sent http_str")
 
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
+          #print("In the loop", i)
           connectionSocket.send(outputdata[i].encode())
-
-        connectionSocket.send("\r\n".encode())
+        #print("left for loop")
+        #connectionSocket.send("\r\n".encode())
         connectionSocket.close()
+        #print("socket closed")
       except IOError:
         # Send response message for file not found (404)
+        #print("IOError")
+        http_str = "HTTP/1.1 404 Not Found\r\n\r\n"
         #Fill in start
-
+        connectionSocket.send(http_str.encode())
+        http_str = "404 Not Found \r\n"
+        connectionSocket.send(http_str.encode())
         #Fill in end
 
 
         #Close client socket
         #Fill in start
-
+        connectionSocket.close()
         #Fill in end
 
     except (ConnectionResetError, BrokenPipeError):
+      #print("connection err")
       pass
+    finally:
+      f.close()
 
   serverSocket.close()
   sys.exit()  # Terminate the program after sending the corresponding data
